d6c0cf3c12a99bd9c32f4e8d116823e517e9f55e
David Roundy <roundyd@physics.oregonstate.edu>
2017-08-09 07:43:11 -0700

    implement queueing daemon

---
+++ src/bin/rq.rs
@@ -0,0 +1,110 @@
+extern crate clap;
+
+extern crate roundqueue;
+
+use std::io::Result;
+
+fn main() {
+    let m = clap::App::new("rq")
+        .subcommand(
+            clap::SubCommand::with_name("run")
+                .about("submit a new job")
+                .arg(clap::Arg::with_name("cores")
+                     .short("c")
+                     .long("cpus")
+                     .takes_value(true)
+                     .value_name("CPUS")
+                     .default_value("1")
+                     .hide_default_value(true)
+                     .help("the number of cores the job requires"))
+                .arg(clap::Arg::with_name("jobname")
+                     .short("J")
+                     .long("job-name")
+                     .takes_value(true)
+                     .value_name("NAME")
+                     .default_value("")
+                     .hide_default_value(true)
+                     .help("the name of the job"))
+                .arg(clap::Arg::with_name("output")
+                     .short("o")
+                     .long("output")
+                     .takes_value(true)
+                     .value_name("OUTFILE")
+                     .default_value("round-queue.out")
+                     .help("the file for stdout and stderr"))
+                .arg(clap::Arg::with_name("verbose")
+                     .long("verbose")
+                     .short("v")
+                     .multiple(true)
+                     .help("show verbose output"))
+                .arg(clap::Arg::with_name("command")
+                     .index(1)
+                     .multiple(true)
+                     .help("command line"))
+        )
+        .subcommand(
+            clap::SubCommand::with_name("q")
+                .about("show the queue")
+                .arg(clap::Arg::with_name("verbose")
+                     .long("verbose")
+                     .short("v")
+                     .multiple(true)
+                     .help("show verbose output"))
+        )
+        .subcommand(
+            clap::SubCommand::with_name("daemon")
+                .about("spawn the runner daemon")
+                .arg(clap::Arg::with_name("verbose")
+                     .long("verbose")
+                     .short("v")
+                     .multiple(true)
+                     .help("show verbose output"))
+        )
+        .get_matches();
+    match m.subcommand() {
+        ("q", _) => {
+            do_q().unwrap();
+        },
+        ("daemon", Some(_)) => {
+            do_daemon().unwrap();
+        },
+        (_, None) => {
+            do_q().unwrap();
+        }
+        ("run", Some(m)) => {
+            let mut command = Vec::new();
+            if let Some(c) = m.values_of("command") {
+                for x in c {
+                    command.push(String::from(x));
+                }
+            }
+            let mut jn = String::from(m.value_of("jobname").unwrap());
+            if jn == "" {
+                jn = command.join(" ");
+            }
+            println!("submitted {:?}", &jn);
+            roundqueue::Job::new(command, jn,
+                                 std::path::PathBuf::from(m.value_of("output").unwrap())
+            ).unwrap().submit().unwrap()
+        },
+        (x, _) => {
+            eprintln!("Invalid subcommand {}!", x);
+            std::process::exit(1);
+        }
+    }
+}
+
+fn do_q() -> Result<()> {
+    let status = roundqueue::Status::new().unwrap();
+    for j in status.waiting.iter() {
+        println!("W {:?} {}", &j.home_dir, &j.jobname);
+    }
+    for j in status.running.iter() {
+        println!("R {:?} {}", &j.home_dir, &j.jobname);
+    }
+    Ok(())
+}
+
+fn do_daemon() -> Result<()> {
+    roundqueue::spawn_runner()
+}
--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,19 @@ extern crate serde_derive;
 
 extern crate serde;
 extern crate serde_json;
+extern crate hostname;
+extern crate unix_daemonize;
+extern crate num_cpus;
+extern crate notify;
+extern crate libc;
+
+use notify::{Watcher};
 
 use std::path::{Path,PathBuf};
 use std::time::{SystemTime,Duration,UNIX_EPOCH};
 use std::io::{Result,Write,Read};
-use std::ffi::OsString;
+
+use std::os::unix::io::{FromRawFd,IntoRawFd};
 
 #[derive(Serialize, Deserialize, Debug, Clone, Eq, PartialEq, Ord, PartialOrd)]
 pub struct Running {
@@ -19,40 +27,68 @@ pub struct Running {
 pub struct Job {
     pub home_dir: PathBuf,
     pub directory: PathBuf,
-    pub command: Vec<OsString>,
+    pub command: Vec<String>,
     pub jobname: String,
+    pub output: PathBuf,
     pub submitted: Duration,
     pub running: Option<Running>,
 }
 
 impl Job {
-    pub fn new(cmd: Vec<OsString>, jobname: String) -> Result<Job> {
+    pub fn new(cmd: Vec<String>, jobname: String, output: PathBuf) -> Result<Job> {
         Ok(Job {
             directory: std::env::current_dir()?,
             home_dir: std::env::home_dir().unwrap(),
             command: cmd,
             jobname: jobname,
+            output: output,
             submitted: SystemTime::now().duration_since(UNIX_EPOCH).unwrap(),
             running: None,
         })
     }
     fn filename(&self) -> PathBuf {
-        PathBuf::from(format!("{}-{}.job",
+        PathBuf::from(format!("{}.{}.job",
                               self.submitted.as_secs(), self.submitted.subsec_nanos()))
+    }
+    fn filepath(&self, subdir: &Path) -> PathBuf {
+        self.home_dir.join(RQ).join(subdir).join(self.filename())
+    }
+    fn is_running_on(&self, host: &str) -> bool {
+        if let Some(ref r) = self.running {
+            return r.node == host
+        }
+        false
     }
     fn read(fname: &Path) -> Result<Job> {
         let mut f = std::fs::File::open(fname)?;
         let mut data = Vec::new();
         f.read_to_end(&mut data)?;
         match serde_json::from_slice::<Job>(&data) {
-            Ok(job) => Ok(job),
+            Ok(job) => {
+                if job.command.len() == 0 {
+                    Err(std::io::Error::new(std::io::ErrorKind::Other, "empty cmd?"))
+                } else {
+                    Ok(job)
+                }
+            },
             Err(e) => Err(std::io::Error::new(std::io::ErrorKind::Other, e)),
         }
     }
     fn save(&self, subdir: &Path) -> Result<()> {
-        let p = self.home_dir.join(RQ).join(subdir).join(self.filename());
-        let mut f = std::fs::File::create(&p)?;
+        let mut f = std::fs::File::create(self.filepath(subdir))?;
         f.write_all(&serde_json::to_string(self).unwrap().as_bytes())
+    }
+    fn change_status(&self, old_subdir: &Path, new_subdir: &Path) -> Result<()> {
+        std::fs::rename(self.filepath(old_subdir), self.filepath(new_subdir))
+    }
+    pub fn submit(&self) -> Result<()> {
+        self.ensure_directories()?;
+        self.save(Path::new(WAITING))
+    }
+    fn ensure_directories(&self) -> Result<()> {
+        std::fs::create_dir_all(&self.home_dir.join(RQ).join(WAITING))?;
+        std::fs::create_dir_all(&self.home_dir.join(RQ).join(RUNNING))?;
+        std::fs::create_dir_all(&self.home_dir.join(RQ).join(COMPLETED))
     }
 }
 
@@ -61,6 +97,7 @@ const RUNNING: &'static str = "RUNNING";
 const WAITING: &'static str = "WAITING";
 const COMPLETED: &'static str = "COMPLETED";
 
+#[derive(Serialize, Deserialize, Debug, Clone, Eq, PartialEq, Ord, PartialOrd)]
 pub struct Status {
     pub waiting: Vec<Job>,
     pub running: Vec<Job>,
@@ -77,7 +114,7 @@ impl Status {
         for userdir in std::fs::read_dir("/home")? {
             if let Ok(userdir) = userdir {
                 let rqdir = userdir.path().join(RQ);
-                eprintln!("{:?}", rqdir);
+                // eprintln!("{:?}", rqdir);
                 if let Ok(rr) = rqdir.join(RUNNING).read_dir() {
                     for run in rr.flat_map(|r| r.ok()) {
                         if let Ok(j) = Job::read(&run.path()) {
@@ -109,11 +146,136 @@ impl Status {
         }
         Ok(status)
     }
+    /// run_next consumes the status to enforce that we must re-read
+    /// the status before attempting anything else.  This is because
+    /// another node may have run something or submitted something.
+    fn run_next(self, host: &str) {
+        let waiting: Vec<_> =  self.waiting.iter().map(|j| j.home_dir.clone()).collect();
+        if waiting.len() == 0 { return; }
+        let mut next_homedir = waiting[0].clone();
+        let mut least_running = 100000;
+        for hd in waiting.into_iter() {
+            let count = self.running.iter().filter(|j| &j.home_dir == &hd).count();
+            if count < least_running {
+                next_homedir = hd;
+                least_running = count;
+            }
+        }
+        let mut job = self.waiting[0].clone();
+        let mut earliest_submitted = job.submitted;
+        for j in self.waiting.into_iter() {
+            if j.home_dir == next_homedir && j.submitted < earliest_submitted {
+                earliest_submitted = j.submitted;
+                job = j;
+            }
+        }
+        println!("starting {:?}", &job.jobname);
+        let f = match std::fs::OpenOptions::new()
+            .append(true).open(job.directory.join(&job.output)) {
+                Ok(f) => f,
+                Err(e) => {
+                    println!("Error creating output {:?}: {}",
+                             job.directory.join(&job.output), e);
+                    return;
+                },
+            };
+        if let Err(e) = job.change_status(Path::new(WAITING), Path::new(RUNNING)) {
+            println!("Unable to change status of job {} ({})", &job.jobname, e);
+            return;
+        }
+        job.running = Some(Running {
+            started: SystemTime::now().duration_since(UNIX_EPOCH).unwrap(),
+            node: String::from(host),
+        });
+        if let Err(e) = job.save(Path::new(RUNNING)) {
+            println!("Yikes, unable to save job? {}", e);
+            std::process::exit(1);
+        }
+        std::thread::spawn(move || {
+            let mut cmd = std::process::Command::new(&job.command[0]);
+            let fd = f.into_raw_fd();
+            let stderr = unsafe {
+                std::process::Stdio::from_raw_fd(fd)
+            };
+            let stdout = unsafe {
+                std::process::Stdio::from_raw_fd(fd)
+            };
+            cmd.args(&job.command[1..]).current_dir(&job.directory)
+                .stderr(stderr)
+                .stdout(stdout)
+                .stdin(std::process::Stdio::null());
+            match cmd.status() {
+                Err(e) => {
+                    println!("Error running {:?}: {}", job.command, e);
+                },
+                Ok(st) => {
+                    println!("Done running {:?}: {}", job.jobname, st);
+                }
+            }
+            if let Err(e) = job.change_status(Path::new(RUNNING), Path::new(COMPLETED)) {
+                println!("Unable to change status of completed job {} ({})",
+                         &job.jobname, e);
+                return;
+            }
+        });
+    }
 }
 
-pub fn create_job(directory: PathBuf, command: String) {
+pub fn spawn_runner() -> Result<()> {
+    let home = std::env::home_dir().unwrap();
+    let host = hostname::get_hostname().unwrap();
+
+    if let Ok(pid_old) = read_pid(&home.join(RQ).join(&host)) {
+        unsafe { libc::kill(pid_old, libc::SIGTERM); }
+        std::thread::sleep(Duration::from_secs(2));
+        unsafe { libc::kill(pid_old, libc::SIGKILL); }
+    }
+    let cpus = num_cpus::get_physical();
+    println!("I am spawning a runner for {} with {} cpus in {:?}!",
+             &host, cpus, &home);
+    unix_daemonize::daemonize_redirect(
+        Some(home.join(RQ).join("log")),
+        Some(home.join(RQ).join("log")),
+        unix_daemonize::ChdirMode::ChdirRoot).unwrap();
+    write_pid(&home.join(RQ).join(&host))?;
+    println!("==================\nRestarting runner!");
+    let (notify_tx, notify_rx) = std::sync::mpsc::channel();
+    let mut watcher =
+        notify::watcher(notify_tx,
+                        std::time::Duration::from_secs(1)).unwrap();
+    watcher.watch(home.join(RQ).join(RUNNING),
+                  notify::RecursiveMode::NonRecursive).ok();
+    watcher.watch(home.join(RQ).join(WAITING),
+                  notify::RecursiveMode::NonRecursive).ok();
+    let mut old_status = Status::new().unwrap();
+    loop {
+        let status = Status::new().unwrap();
+        let running = status.running.iter().filter(|j| j.is_running_on(&host)).count();
+        let waiting = status.waiting.iter().count();
+        if old_status != status {
+            println!("Currently using {}/{} cores, with {} jobs waiting.",
+                     running, cpus, waiting);
+        }
+        old_status = status.clone();
+        if cpus > running && waiting > 0 {
+            status.run_next(&host);
+        }
+        notify_rx.recv().unwrap();
+    }
 }
 
-pub fn running_jobs() -> Result<Vec<Job>> {
-    Ok(Vec::new())
+fn read_pid(fname: &Path) -> Result<libc::pid_t> {
+    let mut f = std::fs::File::open(fname)?;
+    let mut data = Vec::new();
+    f.read_to_end(&mut data)?;
+    match serde_json::from_slice::<libc::pid_t>(&data) {
+        Ok(pid) => Ok(pid),
+        Err(e) => Err(std::io::Error::new(std::io::ErrorKind::Other, e)),
+    }
+}
+
+fn write_pid(fname: &Path) -> Result<()> {
+    let pid = unsafe { libc::getpid() };
+    let mut f = std::fs::File::create(fname)?;
+    f.write_all(&serde_json::to_string(&pid).unwrap().as_bytes())
 }
